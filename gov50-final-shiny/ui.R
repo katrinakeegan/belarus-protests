e52a6b3508caf8b45efe94a70cee97d4b5e22eaf
Katrina Keegan <katrinakeegan@g.harvard.edu>
2020-10-14 14:19:19 -0400

    Nexta connections data cleaning complete

--- gov50-final-shiny/ui.R
+++ gov50-final-shiny/ui.R
@@ -1,33 +1,20 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
+library(shinythemes)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
-))
+shinyUI(
+    navbarPage(theme = shinytheme("journal"), 
+               "Social Media during Belarus Protests",
+               tabPanel("Page1",
+                        h1("Here's a big header"),
+                        p("Here's some words.", a("Here's a link.", href = "http://www.google.com")), p("Here's some more text
+                        and still more text")
+                        
+                        #All the stuff
+                        ),
+               
+               tabPanel(
+                   "Page2"
+               )
+    ))
