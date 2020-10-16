798027456395c0c9c4dc944a7565b08937262e4f
Katrina Keegan <katrinakeegan@g.harvard.edu>
2020-10-15 20:45:47 -0400

    big network created, milestone 4 complete

--- gov50-final-shiny/ui.R
+++ gov50-final-shiny/ui.R
@@ -5,16 +5,23 @@ library(shinythemes)
 
 shinyUI(
     navbarPage(theme = shinytheme("journal"), 
-               "Social Media during Belarus Protests",
-               tabPanel("Page1",
-                        h1("Here's a big header"),
-                        p("Here's some words.", a("Here's a link.", href = "http://www.google.com")), p("Here's some more text
-                        and still more text")
+               "Telegram leadership during 2020 Belarus Protests",
+               tabPanel("About the project",
+                        h1("Who is leading the Belarus protests?"),
+                        p("The goal of this project is to map the network of telegram channels that are leading mass protests in Belarus.")
+                        #a("Here's a link.", href = "http://www.google.com")), p("Here's some more text")
                         
                         #All the stuff
                         ),
+               tabPanel("Most important channels",
+                        plotOutput("mostConnected")
+                        
+                        ),
                
-               tabPanel(
-                   "Page2"
+               tabPanel( "About me",
+                         p("I'm a Master's student at Harvard in Russian, Eastern European and Central Asian Studies."),
+                         p("The github repo for this project can be found", a("here", href = "https://github.com/katrinakeegan/gov50-final"))
+                         
+                         
                )
     ))
